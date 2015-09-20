02137321c5947f37e55057b8cf75ac036efa04ae
Lucien Gaitskell <lucien@gaitskell.com>
2015-09-20 19:52:37 -0400

    updated drive for spin (not functional)

--- FtcRobotController/src/main/java/org/wheeler/robotics/minibotDrive.java
+++ FtcRobotController/src/main/java/org/wheeler/robotics/minibotDrive.java
@@ -9,12 +9,85 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 public class minibotDrive extends OpMode {
     DcMotor leftMotor;
     DcMotor rightMotor;
-    public void init(){
+    boolean oldLeftBumper=false;
+    boolean leftBumper;
+    boolean oldRightBumper=false;
+    boolean rightBumper;
+    double oldLeftJoystick=0;
+    double leftJoystick;
+    double oldRightJoystick=0;
+    double rightJoystick;
+    boolean autoMode=false;
+    boolean oldAutoMode=false;
+
+    private void autoChange(){
+        if(autoMode && autoMode!=oldAutoMode){
+            leftMotor.setPower(0);
+            rightMotor.setPower(0);
+        }
+        oldAutoMode=autoMode;
+    }
+    private void noScope360(boolean left){
+        int multiplier=0;
+        double spinSpeed=1;
+        autoMode=true;
+
+        if (left) {
+            multiplier = -1;
+        }
+        else{
+            multiplier=1;
+        }
+
+        leftMotor.setPower(multiplier*spinSpeed);
+        telemetry.addData("leftAuto", multiplier * spinSpeed);
+        rightMotor.setPower(multiplier * spinSpeed * -1);
+        telemetry.addData("rightAuto",multiplier*spinSpeed*-1);
+    }
+
+    public void right360NoScope(){
+        noScope360(false);
+    }
+
+    public void left360NoScope(){
+        noScope360(true);
+    }
+
+    public void init() {
         leftMotor = hardwareMap.dcMotor.get("left");
         rightMotor = hardwareMap.dcMotor.get("right");
     }
     public void loop(){
-        leftMotor.setPower(gamepad1.left_stick_y);
-        rightMotor.setPower(gamepad1.right_stick_y*-1);
+        telemetry.addData("autoMode",autoMode);
+        leftJoystick=gamepad1.left_stick_y;
+        telemetry.addData("left",leftJoystick);
+        if (oldLeftJoystick!=leftJoystick){
+            autoChange();
+            leftMotor.setPower(leftJoystick);
+            telemetry.addData("leftMotor", leftJoystick);
+            oldLeftJoystick=leftJoystick;
+            autoMode=false;
+        }
+
+        rightJoystick=gamepad1.right_stick_y*-1;
+        telemetry.addData("right", rightJoystick);
+        if (oldRightJoystick!=rightJoystick){
+            autoChange();
+            rightMotor.setPower(rightJoystick);
+            telemetry.addData("leftMotor", rightJoystick);
+            oldRightJoystick=rightJoystick;
+
+        }
+
+        leftBumper=gamepad1.left_bumper;
+        rightBumper=gamepad1.right_bumper;
+        if(leftBumper && leftBumper!=oldLeftBumper){
+            left360NoScope();
+            oldLeftBumper=leftBumper;
+        }
+        else if(rightBumper && rightBumper!=oldRightBumper){
+            right360NoScope();
+            oldRightBumper=rightBumper;
+        }
     }
 }
