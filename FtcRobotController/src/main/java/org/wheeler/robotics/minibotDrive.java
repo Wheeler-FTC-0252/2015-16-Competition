7af76fe7a3ec65ac1d6e880bbcdddd5fe77e056c
Lucien Gaitskell <lucien@gaitskell.com>
2015-10-19 19:06:13 -0400

    added gain

--- FtcRobotController/src/main/java/org/wheeler/robotics/minibotDrive.java
+++ FtcRobotController/src/main/java/org/wheeler/robotics/minibotDrive.java
@@ -13,6 +13,7 @@ public class minibotDrive extends OpMode {
     double leftJoystick;
     double oldRightJoystick=0;
     double rightJoystick;
+    double joystickGain=0.5;
 
     public void init(){
         leftMotors= new DcMotor[] {hardwareMap.dcMotor.get("fL"),hardwareMap.dcMotor.get("bL")};
@@ -22,8 +23,8 @@ public class minibotDrive extends OpMode {
         leftJoystick=gamepad1.left_stick_y;
         telemetry.addData("left",leftJoystick);
         if (oldLeftJoystick!=leftJoystick){
-            leftMotors[0].setPower(leftJoystick);
-            leftMotors[1].setPower(leftJoystick);
+            leftMotors[0].setPower(leftJoystick*joystickGain);
+            leftMotors[1].setPower(leftJoystick*joystickGain);
             telemetry.addData("leftMotor", leftJoystick);
             oldLeftJoystick=leftJoystick;
         }
@@ -31,8 +32,8 @@ public class minibotDrive extends OpMode {
         rightJoystick=gamepad1.right_stick_y*-1;
         telemetry.addData("right", rightJoystick);
         if (oldRightJoystick!=rightJoystick){
-            rightMotors[0].setPower(rightJoystick);
-            rightMotors[1].setPower(rightJoystick);
+            rightMotors[0].setPower(rightJoystick*joystickGain);
+            rightMotors[1].setPower(rightJoystick*joystickGain);
             telemetry.addData("leftMotor", rightJoystick);
             oldRightJoystick=rightJoystick;
         }
