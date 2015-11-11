4628b3dae5f6f612c7f358b3918d61df24fd952f
Lucien Gaitskell <lucien@gaitskell.com>
2015-11-11 18:04:25 -0500

    added VALUES

--- FtcRobotController/src/main/python/ultrasoundSensor.py
+++ FtcRobotController/src/main/python/ultrasoundSensor.py
@@ -3,6 +3,7 @@ import time
 
 DEFAULT={'busNumb':1, 'address':70}
 ADDRESSES={'write':224,'read':225}
+VALUES={'readStart':81}
 
 class ultrasoundSensor:
     def __init__(self, address=DEFAULT['address'], busNumb=DEFAULT['busNumb']):
@@ -10,6 +11,6 @@ class ultrasoundSensor:
         self.ADDRESS=address
 
     def readDistance():
-        self.BUS.write_byte_data(self.ADDRESS, ADDRESSES['write'], 81)
+        self.BUS.write_byte_data(self.ADDRESS, ADDRESSES['write'], VALUES['readStart'])
         time.sleep(0.1)
         return self.BUS.read_byte_data(self.ADDRESS, ADDRESSES['read'])
