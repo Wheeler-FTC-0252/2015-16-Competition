a96af3bd043b55665afa1f4b36d3b9b8ed1da6e4
Lucien Gaitskell <lucien@gaitskell.com>
2015-11-10 22:32:33 -0500

    added initial i2c python code

---
+++ FtcRobotController/src/main/python/ultrasoundSensor.py
@@ -0,0 +1,15 @@
+import smbus
+import time
+
+DEFAULT={'busNumb':1, 'address':0x29}
+ADDRESSES={'write':224,'read':225}
+
+class ultrasoundSensor:
+    def __init__(self, address=DEFAULT['address'], busNumb=DEFAULT['busNumb']):
+        self.BUS = smbus.SMBus(busNumb)
+        self.ADDRESS=address
+
+    def readDistance():
+        self.BUS.write_byte_data(self.ADDRESS, ADDRESSES['write'], 81)
+        time.sleep(0.1)
+        return self.BUS.read_byte_data(self.ADDRESS, ADDRESSES['read'])
