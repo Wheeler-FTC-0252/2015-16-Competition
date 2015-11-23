74cbc68d9fd2a789d42f6186383179da764dcc13
Lucien Gaitskell <lucien@gaitskell.com>
2015-11-22 20:34:10 -0500

    using harwareMap instead of new instantiation of I2cDevice (LMID)

--- FtcRobotController/src/main/java/org/wheeler/robotics/i2c/LegacyModuleI2cDevice.java
+++ FtcRobotController/src/main/java/org/wheeler/robotics/i2c/LegacyModuleI2cDevice.java
@@ -1,5 +1,6 @@
 package org.wheeler.robotics.i2c;
 
+import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.I2cController;
 import com.qualcomm.robotcore.hardware.I2cDevice;
 import com.qualcomm.robotcore.hardware.LegacyModule;
@@ -21,18 +22,16 @@ public class LegacyModuleI2cDevice implements I2cController.I2cPortReadyCallback
     private final Lock readLock;
     private final byte[] writeCache;
     private final Lock writeLock;
-    private final int port;
     private final I2cDevice i2cDevice;
     private final int i2cAddress;
     private final int IO_START = 4;
 
-    public LegacyModuleI2cDevice(LegacyModule legacyModule, int physicalPort, int i2cAddress) {
-        this.i2cDevice = new I2cDevice(legacyModule,physicalPort);
+    public LegacyModuleI2cDevice(HardwareMap hardwareMap, String deviceName, int i2cAddress) {
+        this.i2cDevice =  hardwareMap.i2cDevice.get(deviceName);
         this.readCache = i2cDevice.getI2cReadCache();
         this.readLock = i2cDevice.getI2cReadCacheLock();
         this.writeCache = i2cDevice.getI2cWriteCache();
         this.writeLock = i2cDevice.getI2cWriteCacheLock();
-        this.port = physicalPort;
         this.i2cAddress=i2cAddress;
         i2cDevice.registerForI2cPortReadyCallback(this);
     }
