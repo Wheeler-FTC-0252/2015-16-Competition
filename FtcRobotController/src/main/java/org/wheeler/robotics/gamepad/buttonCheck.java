c5c44a960253786b986d392bd8e86ee75b7fc859
Lucien Gaitskell <lucien@gaitskell.com>
2015-12-03 19:07:23 -0500

    added buttonCheck

---
+++ FtcRobotController/src/main/java/org/wheeler/robotics/gamepad/buttonCheck.java
@@ -0,0 +1,21 @@
+package org.wheeler.robotics.gamepad;
+
+/**
+ * Created by lucien on 12/3/15.
+ *
+ * @author Lucien Gaitskell
+ * @version 0.1
+ */
+public final class buttonCheck {
+    private buttonCheck(){} //don't want instantiation
+
+     public static boolean checkButton(boolean buttonValue, Boolean previousValue){
+         boolean returnVal = false;
+         if (buttonValue!=previousValue) {
+             returnVal = true;
+         }
+         previousValue = buttonValue;
+
+         return buttonValue;
+     }
+}
