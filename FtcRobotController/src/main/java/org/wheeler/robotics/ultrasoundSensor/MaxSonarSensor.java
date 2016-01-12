c3106e4ea09bf9b308ae0a3a9114347805689707
Lucien Gaitskell <lucien@gaitskell.com>
2016-01-12 06:44:32 -0500

    updated MaxSonarSensor

--- FtcRobotController/src/main/java/org/wheeler/robotics/ultrasoundSensor/MaxSonarSensor.java
+++ FtcRobotController/src/main/java/org/wheeler/robotics/ultrasoundSensor/MaxSonarSensor.java
@@ -12,15 +12,16 @@ import org.wheeler.robotics.ollie.Wire;
  * @author Lucien Gaitskell
  * @version 0.1
  */
-public class MaxSonarSensor implements Runnable {
+public class MaxSonarSensor extends Thread {
     private Wire ds;
     private int readCount=0;
     private int distance;
     private int returnDistance=0;
     private int previousDistance=0;
     private long pingTime;
-    private boolean running=true;
+    public boolean running=true;
     public boolean log=false;
+    public String logTag = "distance";
 
     public MaxSonarSensor(HardwareMap hardwareMap, String name, int address){
         ds = new Wire(hardwareMap, name, address);
@@ -33,40 +34,44 @@ public class MaxSonarSensor implements Runnable {
     }
 
     public void run() {
-        ds.beginWrite(0x51);
-        ds.write(0);
+        ds.beginWrite(224);
+        ds.write(81);
         ds.endWrite();
         pingTime = System.currentTimeMillis();
 
         while (running) {
-            if ((System.currentTimeMillis() - pingTime) > 100) {
-                ds.requestFrom(0, 2);
-                ds.beginWrite(0x51);
-                ds.write(0);
-                ds.endWrite();
-                pingTime = System.currentTimeMillis();
-            }
+            read();
+        }
+    }
 
-            if (ds.responseCount() > 0) {
-                ds.getResponse();
-                if (ds.isRead()
-                        ) {
-                    long micros = ds.micros();
-                    distance = ds.readHL();
-                    if (distance < 760) {
-                        readCount++;
-                        returnDistance=distance;
+    private synchronized void read() {
+        if ((System.currentTimeMillis() - pingTime) > 100) {
+            ds.requestFrom(225, 2);
+            ds.beginWrite(224);
+            ds.write(81);
+            ds.endWrite();
+            pingTime = System.currentTimeMillis();
+        }
 
-                        if (log){
-                            Log.d("distance", String.valueOf(distance));
+        if (ds.responseCount() > 0) {
+            ds.getResponse();
+            if (ds.isRead()
+                    ) {
+                long micros = ds.micros();
+                distance = ds.readHL();
+                if (distance < 760) {
+                    readCount++;
+                    returnDistance=distance;
 
-                            int change = Math.abs(previousDistance-returnDistance);
-                            if (change>20) {
-                                Log.d("distanceWarn", "Thats a big change! (" + String.valueOf(change) + ")");
-                            }
+                    if (log){
+                        Log.d(logTag, String.valueOf(distance));
+
+                        int change = Math.abs(previousDistance-returnDistance);
+                        if (change>20) {
+                            Log.d("distanceWarn", "Thats a big change! (" + String.valueOf(change) + ")");
                         }
-                        previousDistance=returnDistance;
                     }
+                    previousDistance=returnDistance;
                 }
             }
         }
