2b425938564216dc6d50bf0e9557eb7843f1156f
Lucien Gaitskell <lucien@gaitskell.com>
2016-02-05 21:36:19 -0500

    changing AdafruitColorSensorTest to use MR library

--- FtcRobotController/src/main/java/org/wheeler/robotics/rgbSensor/AdafruitColorSensorTest.java
+++ FtcRobotController/src/main/java/org/wheeler/robotics/rgbSensor/AdafruitColorSensorTest.java
@@ -1,11 +1,7 @@
 package org.wheeler.robotics.rgbSensor;
 
-import android.util.Log;
-
-import com.qualcomm.ftcrobotcontroller.opmodes.AdafruitRGBExample;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
-
-import org.wheeler.robotics.library.ollie.Wire;
+import com.qualcomm.robotcore.hardware.ColorSensor;
 
 /**
  * Created by lucien on 1/24/16.
@@ -13,59 +9,16 @@ import org.wheeler.robotics.library.ollie.Wire;
  * @author Lucien Gaitskell
  * @version 0.1
  */
-
 public class AdafruitColorSensorTest extends OpMode {
-	Wire cs;
+	ColorSensor cs;
+
 	public void init() {
-		cs = new Wire(hardwareMap, "cs", 0x29);
-	}
-
-	public void start() {
-		Log.d("ACSensorStatus", "Request Version from Color Sensor");
-
-		cs.requestFrom(0x80 | 0x12, 1);
-		while(cs.responseCount() < 1) {
-			Log.d("ACSensorStatus", "Waiting for response (" + String.valueOf(cs.responseCount()) + ")");
-		}
-
-		cs.getResponse();
-
-		while (!cs.isRead()) {
-			cs.getResponse();
-			Log.d("ACSensorStatus", "Not Read Mode");
-		}
-
-		Log.d("ACSensorStatus", "Reading from Color Sensor");
-
-		int ver = cs.read();
-		if (ver == 0x44){
-			Log.d("ACSensorStatus", "DEVICE FOUND");
-			cs.beginWrite(0x80 | 0x00);
-			cs.write(0x01 | 0x02);
-			cs.endWrite();
-
-			cs.beginWrite(0x80 | 0x14);
-			cs.endWrite();
-		}
-		else {
-			Log.d("ACSensorStatus", "DEVICE FAILED (" + String.valueOf(ver) + ")");
-		}
-
+		cs = hardwareMap.colorSensor.get("cs");
 	}
 
 	public void loop() {
-		cs.requestFrom(0x17, 2);
-		while(cs.responseCount() < 1) {}
-		cs.getResponse();
-		while (!cs.isRead()) {
-			cs.getResponse();
-		}
-		Log.d("ACSensorStatus", "red: " + String.valueOf(cs.readHL()));
-
-		try {
-			Thread.sleep(500);
-		} catch (InterruptedException e) {
-			e.printStackTrace();
-		}
+		telemetry.addData("red", cs.red());
+		telemetry.addData("green", cs.green());
+		telemetry.addData("blue", cs.blue());
 	}
 }
