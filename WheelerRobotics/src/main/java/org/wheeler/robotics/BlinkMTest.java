6000cacd767c8d144b7437d4285cfafd53644f7b
luciengaitskell <luciengaitskell@users.noreply.github.com>
2016-03-05 19:24:10 -0500

    Adding sensor test files

---
+++ WheelerRobotics/src/main/java/org/wheeler/robotics/BlinkMTest.java
@@ -0,0 +1,33 @@
+package org.wheeler.robotics;
+
+import android.util.Log;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import org.swerverobotics.library.ClassFactory;
+import org.swerverobotics.library.interfaces.II2cDeviceClient;
+import org.swerverobotics.library.interfaces.TeleOp;
+
+/**
+ * Created by lucieng on 3/5/16.
+ */
+
+@TeleOp()
+public class BlinkMTest extends LinearOpMode {
+    II2cDeviceClient led;
+
+    public void runOpMode() throws InterruptedException {
+        led = ClassFactory.createI2cDeviceClient(this, hardwareMap.i2cDevice.get("lidar"), 0x00, false);
+        led.engage();
+        waitOneFullHardwareCycle();
+        Log.d("lidar", "LED Address: " + led.read8(0x61));
+        waitForStart();
+        led.write8(0x6f, 0);
+        waitOneFullHardwareCycle();
+        while (this.opModeIsActive()) {
+            led.write(0x6e, new byte[]{5, 5, 5});
+        }
+        led.disengage();
+        led.close();
+    }
+}
---
+++ WheelerRobotics/src/main/java/org/wheeler/robotics/lidarlite/LidarLiteTest.java
@@ -0,0 +1,53 @@
+package org.wheeler.robotics.lidarlite;
+
+import android.util.Log;
+
+import  com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.util.TypeConversion;
+
+import org.swerverobotics.library.ClassFactory;
+import org.swerverobotics.library.interfaces.II2cDeviceClient;
+import org.swerverobotics.library.interfaces.TeleOp;
+import org.wheeler.robotics.library.ollie.Wire;
+
+/**
+ * Created by lucieng on 2/25/16.
+ */
+
+@TeleOp()
+public class LidarLiteTest extends LinearOpMode {
+	II2cDeviceClient ls;
+	long writeTime;
+
+	public void runOpMode() throws InterruptedException {
+		this.ls = ClassFactory.createI2cDeviceClient(this, ClassFactory.createI2cDevice(hardwareMap.i2cDevice.get("lidar")), 0x62, false);
+
+		waitForStart();
+		ls.engage();
+		waitOneFullHardwareCycle();
+
+		Log.d("readValue", "Version Number: " + String.valueOf(ls.read8(0x41)));
+		Log.d("readValue", "I2c Address: " + String.valueOf(ls.getI2cAddr()));
+
+		while (this.opModeIsActive()) {
+			ls.write8(0x00, 0x04);
+			writeTime = System.currentTimeMillis();
+			waitOneFullHardwareCycle();
+			while (System.currentTimeMillis()-writeTime<1000 && this.opModeIsActive()) {}
+			byte[] value=ls.read(0x10,2);
+			Log.d("readValue", "length: " + String.valueOf(value.length)
+					+ ", distance0: " + String.valueOf(value[0])
+					+ ", distance1: " + String.valueOf(value[1]));
+			telemetry.addData("distanceLength", value.length);
+			telemetry.addData("distance0", value[0]);
+			telemetry.addData("distance1", value[1]);
+
+			waitOneFullHardwareCycle();
+			//TODO: try reading version number
+			//telemetry.addData("distance", TypeConversion.byteArrayToInt(ls.read(0x0f,2)));
+		}
+
+		ls.disengage();
+		ls.close();
+	}
+}
